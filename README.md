29e712cf182b1ed5dc70e5391f39832d631de1e0
ziviledauderiene <zivile.dauderiene@gmail.com>
2022-02-22 17:33:38 +0200

    aside styling done. toggle dark/light mode implemented

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Invoice app
+
+This is a solution to the [Invoice app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/invoice-app-i7KaLTQjl). 
+I am working on it!
+
 ### Link
 https://invoice-app-ziviledauderiene.vercel.app/
+
+### The challenge
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Create, read, update, and delete invoices
+- Receive form validations when trying to create/edit an invoice
+- Save draft invoices, and mark pending invoices as paid
+- Filter invoices by status (draft/pending/paid)
+- Toggle light and dark mode
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS
+- [React](https://reactjs.org/) - JS library
--- src/Aside.js
+++ src/Aside.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import avatar from "./assets/image-avatar.jpg";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+import { ReactComponent as MoonIcon } from "./assets/icon-moon.svg";
+import { ReactComponent as SunIcon } from "./assets/icon-sun.svg";
 
 export const Aside = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+  };
+
   return (
-    <aside>Aside</aside>
-  )
-}
+    <aside>
+      <div className="logo">
+        <Logo />
+        <div className="empty"></div>
+      </div>
+      <div className="aside-bottom">
+        <div className="mode-toogle" onClick={toggleDarkMode}>
+          {darkMode ? <SunIcon /> : <MoonIcon />}
+        </div>
+        <div className="avatar">
+          <img src={avatar} alt="avatar" width={40} />
+        </div>
+      </div>
+    </aside>
+  );
+};
